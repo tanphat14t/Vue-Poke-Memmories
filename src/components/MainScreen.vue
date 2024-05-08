951373885858b29cf951524608dc12266c35a0c0
Lucius <tanphat14t@gmail.com>
2024-05-08 09:48:25 +0700

    style layot

--- src/components/MainScreen.vue
+++ src/components/MainScreen.vue
@@ -4,20 +4,20 @@
     <h3>Select mode to start game</h3>
     <div class="actions">
       <button class="button" @click="onStart(16)">
-        <span>4x4</span>
-        <span>Easy</span>
+        <span class="number">4x4</span>
+        <span class="text">Easy</span>
       </button>
       <button class="button" @click="onStart(36)">
-        <span>6x6</span>
-        <span>Normal</span>
+        <span class="number">6x6</span>
+        <span class="text">Normal</span>
       </button>
       <button class="button" @click="onStart(64)">
-        <span>8x8</span>
-        <span>Hard</span>
+        <span class="number">8x8</span>
+        <span class="text">Hard</span>
       </button>
       <button class="button" @click="onStart(100)">
-        <span>10x10</span>
-        <span>Super Hard</span>
+        <span class="number">10x10</span>
+        <span class="text">Super Hard</span>
       </button>
     </div>
   </div>
@@ -32,4 +32,55 @@ export default {
   },
 };
 </script>
-<style lang="css"></style>
+<style lang="css" scoped>
+.screen {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: var(--dark);
+  width: 100%;
+  height: 100vh;
+  color: var(--light);
+}
+h1 {
+  font-size: 72px;
+  line-height: normal;
+  margin-bottom: 12px;
+}
+h3 {
+  font-size: 32px;
+  line-height: normal;
+  margin-bottom: 16px;
+}
+.actions {
+  display: flex;
+  gap: 2rem;
+  flex-wrap: nowrap;
+  max-width: 1170px;
+  width: 100%;
+  margin: 0 auto;
+}
+button {
+  width: calc(100% / 4 - 2rem);
+  border: 1px solid var(--light);
+  border-radius: 1rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  aspect-ratio: 1;
+  cursor: pointer;
+}
+button span.number {
+  font-family: var(--font);
+  font-size: 32px;
+  line-height: normal;
+  margin-bottom: 8px;
+}
+button span.text {
+  font-family: var(--font);
+  font-size: 20px;
+  line-height: normal;
+}
+</style>
--- src/components/ResultScreen.vue
+++ src/components/ResultScreen.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="screen">
-    <h1>Congratulation</h1>
+    <h1>✨ Congratulations ✨</h1>
     <p>{{ Math.round(timer / 1000) }} Second</p>
     <button @click="onStartAgain">Start Again</button>
   </div>
@@ -23,4 +23,44 @@ export default {
   },
 };
 </script>
-<style lang="css" scoped></style>
+<style lang="css" scoped>
+.screen {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: var(--dark);
+  width: 100%;
+  height: 100vh;
+  color: var(--light);
+}
+h1 {
+  font-size: 72px;
+  line-height: normal;
+  margin-bottom: 12px;
+}
+p {
+  font-size: 32px;
+  line-height: normal;
+  margin-bottom: 16px;
+}
+
+button {
+  width: 120px;
+  height: 52px;
+  border: 1px solid var(--light);
+  border-radius: 1rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  font-family: var(--font);
+  font-size: 20px;
+  transition: all 0.4s;
+}
+button:hover {
+  background: transparent;
+  color: var(--light);
+}
+</style>
